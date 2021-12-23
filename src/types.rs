e49684837400e64183370c50997b756fd9014f74
Haroon Syed <51543523+haroonsyed@users.noreply.github.com>
2021-12-22 20:08:02 -0500

    Finished learning about variables and casting

---
+++ src/types.rs
@@ -0,0 +1,17 @@
+pub fn run() {
+    //Primitive types
+    /*
+        Intergers: u8,i8,u16,i16,u32,i32,u64,i64,u128,i128
+        Floats: f32,f64
+        bool
+        char
+        Tuples
+        Arrays
+        Objects
+    */
+    //Explicit typing and casting
+    let bigNum:i64 = (std::i32::MAX as i64)+1;
+    println!("Bignum value: {}",bigNum);
+
+
+}
