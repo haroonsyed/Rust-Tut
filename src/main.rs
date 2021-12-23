04d60645d7ef47b82324397f1aba9ce01bacc98c
Haroon Syed <51543523+haroonsyed@users.noreply.github.com>
2021-12-22 20:21:26 -0500

    Added more notes on String class

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 mod print;
 mod vars;
 mod types;
+mod strings;
+mod arrays;
 
 fn main() {
-    //print::run();
-    //vars::run();
-    types::run();
+    // print::run();
+    // vars::run();
+    // types::run();
+    //strings::run();
+    arrays::run();
 }
--- src/strings.rs
+++ src/strings.rs
@@ -1,3 +1,12 @@
+use std::ops::Add;
+
 pub fn run() {
-    
+    let mut myString = String::from("Hello");
+    println!("Length of myString: {}",myString.len());
+    myString.push_str(" World!");
+    println!("New value of myString: {}",myString);
+    //There are a bunch of obvious functionality in the string class
+
+    //One cool thing is that you can set a max capacity for strings
+    let mut capped = String::with_capacity(5);
 }
