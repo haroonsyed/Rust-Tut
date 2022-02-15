da51b7b0fdb2291434e76862b3a6a5c3d85c372b
Haroon Syed <51543523+haroonsyed@users.noreply.github.com>
2022-02-14 22:14:34 -0500

    Learned closures (anonymous functions)\

--- src/functions.rs
+++ src/functions.rs
@@ -1,6 +1,13 @@
 pub fn run() {
     greeting("Haroon");
-    println!("5+5 is {}", add(5,5))
+    println!("5+5 is {}", add(5,5));
+
+    let sum = add(5,5);
+    println!("Sum of 5,5 numbers from variable sum: {}", sum);
+
+    //Closure (essentially storing an an anonymous function in a variable)
+    let add_nums = | n1:i32,n2:i32| n1+n2;
+    println!("Sum of two nums 3 & 7 using a closure: {}", add_nums(3,7));
 }
 
 //Takes in a pointer to a string variable (pass by ref)
